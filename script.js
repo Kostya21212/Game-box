346783f184002702039894bf36f68f015bf4acc0
Onischenko Kostyantyn <Onischenko411@gmail.com>
2024-04-24 01:30:14 +0300

    reload

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-canvas.width = 1024;
-canvas.height = 567;
-ctx.fillStyle = "red",
-ctx.fillRect(0,0,canvas.width,canvas.height)
+// const canvas = document.getElementById('canvas');
+// const ctx = canvas.getContext('2d');
+// canvas.width = 1024;
+// canvas.height = 567;
+// ctx.fillStyle = "red",
+// ctx.fillRect(0,0,canvas.width,canvas.height);
+// ctx.fillStyle = 'orange';
+// ctx.fillRect(0,0,100,100)
+// let y = 0
+// let y2 = 0
+
+// class player {
+//    constructor(){
+//     this.position = {
+
+//     }
+//    }
+//    draw(){
+//     ctx.fillStyle= 'yellow';
+//     ctx.fillRect(this.position.x, this.position.y, 100, 100)
+//    }
+//    update(){
+//     this,this.draw()
+//     this.position++
+//    }
+// }
+// const player = new Player ({x:0,y:0})
+// const player2 = new Player ({x:0,y:0})
+// function animated (){
+//   let y = 0
+// let y2 = 0
+//   window.requestAnimationFrame(animated);
+//   ctx.fillStyle = "red",
+// ctx.fillRect(0,0,canvas.width,canvas.height);
+//  player.draw();
+//  player.update()
+// }
+// 
+
+
+
+
+
+
+
+
 
 
 // var number=function(array){
@@ -28,3 +68,47 @@ ctx.fillRect(0,0,canvas.width,canvas.height)
 // return res
 // }
 // console.log(arrayDiff([1,2,2,2,3],[2]))
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 567;
+const gravity = 0.5;
+
+class Player{
+    constructor (position){
+        this.position = position;
+        this.velocity = {x: 0,y: 1};
+        this.height = 100;
+    }
+        draw(){
+            ctx.fillStyle = 'red';
+            ctx.fillRect(this.position.x,this.position.y,100,this.height);
+        }
+        update(){
+            this.draw()
+            this.position.y += this.velocity.y;
+            if(this.position.y+this.height+this.velocity.y < canvas.height){
+                this.velocity.y += gravity
+            }else{
+                this.velocity.y = 0
+            }
+        }
+    
+}
+const player = new Player({x:0,y:0,});
+const player2 = new Player({x:200,y:0});
+let y = 0
+let x = 0
+function animate(){
+window.requestAnimationFrame(animate);
+ctx.fillStyle = 'White'
+ctx.fillRect(0,0,canvas.width ,canvas.height);
+player.draw();
+player.update()
+player2.draw()
+player2.update()
+}
+animate()
+
